27fd74751086e5e466fa48515d36afeac9a0a2a9
freymi1856 <144949039+freymi1856@users.noreply.github.com>
2024-01-04 20:18:28 +0300

    Inegers_11.6

--- tasks/integers/tasks/11/solution.h
+++ tasks/integers/tasks/11/solution.h
@@ -1,9 +1,21 @@
 #pragma once
 
-#include <stdint.h>
+#include <math.h>
 #include <stdio.h>
+#include <stdint.h>
 
-int Task() {
-    // write your solution here
-    return 0;
+int64_t Len(int64_t num) {
+    int64_t counter = 0;
+    while (num > 0) {
+        num /= 10;
+        counter += 1;
+    }
+    return counter;
+}
+
+int main() {
+    int64_t num = 56243;
+    int64_t a = Len(num);
+    num = (pow(10, (a - 1)) * (num % 10)) + (num / 10);
+    printf("%ld", num);
 }
